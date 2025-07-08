c4065ed4f8c81d394b8f8833c2a5d91abeb9e97e
OleksandrVolos <o.o.volosonok@student.khai.edu>
2025-07-08 19:12:34 +0300

    Створення класу Калькуляції чаєвих

---
+++ BillCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WpfApp1
+{
+    public static class BillCalculator
+    {
+        private const decimal GstRate = 0.05m;
+
+        public static decimal CalculateNetTotal(List<MenuItem> items)
+        {
+            return items.Sum(item => item.Price);
+        }
+
+        public static decimal CalculateGst(decimal netTotal)
+        {
+            return Math.Round(netTotal * GstRate, 2);
+        }
+
+        public static decimal CalculateTip(decimal netTotal, decimal tipValue, bool isPercentage)
+        {
+            if (isPercentage)
+                return Math.Round(netTotal * tipValue / 100m, 2);
+            return tipValue;
+        }
+
+        public static decimal CalculateTotal(decimal net, decimal tip, decimal gst)
+        {
+            return Math.Round(net + tip + gst, 2);
+        }
+    }
+}
